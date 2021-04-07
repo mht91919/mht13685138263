55ab9334cbc4154e5c85edbe94f51e2cd1f3715d
mht91919 <mht91919@qq.com>
2021-04-07 15:03:39 +0800

    kez

--- 赶集网/租房.py
+++ 赶集网/租房.py
@@ -1,5 +1,10 @@
 import requests
 import parsel
+import csv
+
+f = open('租房信息.csv', mode='a', encoding='gbk', newline='')
+csc_writer = csv.DictWriter(f, fieldnames=['表题', '基本信息', '地区', '价格'])
+csc_writer.writeheader()
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
@@ -18,7 +23,17 @@ for index in divs:
     info_str = ','.join(info_list)
     info_address = index.css('.area a span::text').get()
     # dl下的dd模块 第5个
-    listings = index.css('dl>dd:nth-child(5) span span::text').get()
-    info_href = index.css('.title a::attr(href)').get()
-    print(info_str, info_address, info_href, sep='/')
-    break
+    listings = index.css('dl dd:nth-child(5) span span::text').get().strip()  # 房源
+    info_href = index.css('.title a::attr(href)').get()  # 连接
+    feature = index.css('.feature span::text').get()  # 连接
+    price = index.css('.price .num::text').get() + '元/月'  # 价格
+
+    dic = {
+        '表题': title,
+        '基本信息': info_str,
+        '地区': feature,
+        '价格': price
+    }
+    csc_writer.writerow(dic)
+    # print(title, info_str, info_address, listings, feature, price, info_href,
+    #       sep=' / ')
